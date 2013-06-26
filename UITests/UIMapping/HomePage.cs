95550008fb6a41600d8fd8c1d4083bbaf4dcaef3
NaveenHebbar <naveen.hebbar@happiestminds.com>
2013-06-26 12:44:28 +0530

    Updating TCs

---
+++ UITests/UIMapping/HomePage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CloudPOC.UI_Mapping
+{
+     class HomePage
+    {
+         public const string tbxUrl = "ext-gen1007";
+         public const string btnPushUrl = "button-1016-btnInnerEl";
+       
+  
+    }
+}
