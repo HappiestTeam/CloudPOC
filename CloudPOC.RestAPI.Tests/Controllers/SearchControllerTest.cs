5266ba26ca5b6f26a03a9f68af45c6ab73206d21
ananthacn <anantha4u@gmail.com>
2013-06-26 14:18:05 +0530

    Added unit test for search Signed-off-by: ananthacn <anantha4u@gmail.com>

--- CloudPOC.RestAPI.Tests/Controllers/SearchControllerTest.cs
+++ CloudPOC.RestAPI.Tests/Controllers/SearchControllerTest.cs
@@ -7,21 +7,55 @@ using CloudASPNETWebApi.Controllers;
 using CloudASPNETWebApi.Models;
 using System.Configuration;
 
+using Thrift;
+using CassandraClient = Apache.Cassandra.Cassandra.Client;
+using Apache.Cassandra;
+using Aquiles.Helpers.Encoders;
+using Aquiles.Cassandra10;
+using Aquiles.Core;
+using Aquiles.Core.Configuration;
+using Aquiles.Core.Cluster;
+using Aquiles.Helpers;
+using Thrift.Transport;
+using Thrift.Protocol;
+
+
 
 namespace CloudASPNETWebApi.Tests.Controllers
 {
-    [TestClass,Ignore]
+    [TestClass]
     public class SearchControllerTest
     {
         [TestMethod]
         public void Test_To_Search_Valid_Url_In_Database()
         {
-            string message = ConfigurationManager.AppSettings["SearchValue"];
-            bool actualResult;
+            string url = "Test msg";
+            string  actualResult;
             SearchController searchController = new SearchController();
-            actualResult = searchController.Search(message);
-            Assert.AreEqual(true, actualResult);
+            actualResult = searchController.Search(url);
+            Assert.AreEqual("Invalid URL", actualResult);
         }
 
+
+        [TestMethod]
+        public void Test_To_Check_Cassandra_Service()
+        {
+            bool result;
+            TTransport transport = new TSocket("localhost", 9160);
+            TProtocol protocol = new TBinaryProtocol(transport);
+            Cassandra.Client client = new Cassandra.Client(protocol);
+            try
+            {
+                transport.Open();
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+
+            Assert.AreEqual(true, result);
+
+        }
     }
 }
